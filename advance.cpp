f553a1b607360c4960ea9af100780a91c70ad96f
Gabriel Hellebrand <hellebrandg@gmail.com>
2024-05-15 20:42:12 -0600

    added some functunalitiy to crazy & floater birds with the strategy design pattern

--- advance.cpp
+++ advance.cpp
@@ -28,19 +28,64 @@ void Inertia::advance(Bird bird)
 
 void Gravity::advance(Bird bird)
 {
-   
+    {
+        // gravity
+        v.addDy(-0.07);
+
+        // inertia
+        pt.add(v);
+
+        // out of bounds checker
+        if (isOutOfBounds())
+        {
+            kill();
+            points *= -1; // points go negative when it is missed!
+        }
+    }
 }
 
 
 
 void Buoyancy::advance(Bird bird)
 {
-   
+   // floater
+       // large amount of drag
+    v *= 0.990;
+
+    // inertia
+    pt.add(v);
+
+    // anti-gravity
+    v.addDy(0.05);
+
+    // out of bounds checker
+    if (isOutOfBounds())
+    {
+        kill();
+        points *= -1; // points go negative when it is missed!
+    }
 }
 
 
 
 void Chaos::advance(Bird bird)
 {
-   
+    {
+        // erratic turns eery half a second or so
+        if (randomInt(0, 15) == 0)
+        {
+            v.addDy(randomFloat(-1.5, 1.5));
+            v.addDx(randomFloat(-1.5, 1.5));
+        }
+
+        // inertia
+        pt.add(v);
+
+        // out of bounds checker
+        if (isOutOfBounds())
+        {
+            kill();
+            points *= -1; // points go negative when it is missed!
+        }
+    }
 }
